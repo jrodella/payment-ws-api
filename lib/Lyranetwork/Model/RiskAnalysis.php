69f037ea172fc2b85db5254b270087c5923a2218
Nabil Semaoune <nabil.semaoune@lyra-network.com>
2018-01-24 17:58:17 +0100

    Update Licence notice year.

--- lib/Lyranetwork/Model/RiskAnalysis.php
+++ lib/Lyranetwork/Model/RiskAnalysis.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Copyright (C) 2017 Lyra Network.
+ * Copyright (C) 2017-2018 Lyra Network.
  * This file is part of Lyra payment WS API.
  *
  * See COPYING.txt for license details.
  *
  * @author    Lyra Network <contact@lyra-network.com>
- * @copyright 2017 Lyra Network
+ * @copyright 2017-2018 Lyra Network
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL v3)
  */
 namespace Lyranetwork\Model;
